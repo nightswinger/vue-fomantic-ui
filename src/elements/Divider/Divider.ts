3cde811c6a60d1d40f7ba6c45b5e51f6f5b92912
nightswinger <stardustkids83@gmail.com>
2021-02-18 22:46:56 +0000

    refactor Divider

--- src/elements/Divider/Divider.ts
+++ src/elements/Divider/Divider.ts
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import { computed, defineComponent, h } from "vue";
+import { computeKeyOnly, computeKeyValue } from "../../utils/classNameHelper";
 
 export default defineComponent({
   name: 'SuiDivider',
@@ -10,31 +11,21 @@ export default defineComponent({
     horizontal: Boolean,
     inverted: Boolean,
     section: Boolean,
+    textAlign: String,
     vertical: Boolean
   },
   setup(props) {
-    const {
-      clearing,
-      fitted,
-      hidden,
-      horizontal,
-      inverted,
-      section,
-      vertical
-    } = props
-
     const computedClass = computed(() => {
       return clsx(
         'ui',
-        {
-          clearing,
-          fitted,
-          hidden,
-          horizontal,
-          inverted,
-          section,
-          vertical
-        },
+        computeKeyOnly(props.clearing, 'clearing'),
+        computeKeyOnly(props.fitted, 'fitted'),
+        computeKeyOnly(props.hidden, 'hidden'),
+        computeKeyOnly(props.horizontal, 'horizontal'),
+        computeKeyOnly(props.inverted, 'inverted'),
+        computeKeyOnly(props.section, 'section'),
+        computeKeyOnly(props.vertical, 'vertical'),
+        computeKeyValue(props.textAlign, 'aligned'),
         'divider'
       )
     })
