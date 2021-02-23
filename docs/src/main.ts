a398df3ede0f9308561549a095668bfeca330d51
nightswinger <stardustkids83@gmail.com>
2021-02-23 19:14:52 +0000

    fix docs build error

--- docs/src/main.ts
+++ docs/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import FomanticUI from "../../src"
+import FomanticUI from "vue-fomantic-ui"
 import 'fomantic-ui-css/semantic.min.css'
 
 import 'prismjs'
