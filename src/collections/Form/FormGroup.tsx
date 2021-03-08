27f8c2d0d37f04fe5f470bdf7efdf4b1bc83496c
nightswinger <stardustkids83@gmail.com>
2021-03-08 17:29:48 +0000

    refactor FormGroup

--- src/collections/Form/FormGroup.tsx
+++ src/collections/Form/FormGroup.tsx
@@ -1,10 +1,25 @@
-import { defineComponent } from "vue";
+import clsx from "clsx";
+import { computed, defineComponent } from "vue";
+import { computeKeyValue } from "../../utils/classNameHelper";
 
 export default defineComponent({
   name: 'SuiFormGroup',
+  props: {
+    widths: String
+  },
+  setup(props) {
+    const computedClass = computed(() => {
+      return clsx(
+        computeKeyValue(props.widths, 'width'),
+        'fields'
+      )
+    })
+
+    return { computedClass }
+  },
   render() {
     return (
-      <div class="fields">
+      <div class={this.computedClass}>
         {this.$slots.default?.()}
       </div>
     )
