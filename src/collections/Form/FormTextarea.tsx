0778caaea0d5b9db03a88d1d9f6f50ccbb404ae6
nightswinger <stardustkids83@gmail.com>
2021-03-12 22:07:31 +0000

    add FormTextarea

---
+++ src/collections/Form/FormTextarea.tsx
@@ -0,0 +1,23 @@
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'SuiFormTextarea',
+  props: {
+    label: String,
+    placeholder: String,
+  },
+  render() {
+    if (this.label) {
+      <div class="field">
+        <label>{this.label}</label>
+        <textarea placeholder={this.placeholder}></textarea>
+      </div>
+    }
+
+    return (
+      <div class="field">
+        <textarea placeholder={this.placeholder}></textarea>
+      </div>
+    )
+  }
+})
--- src/collections/Form/index.ts
+++ src/collections/Form/index.ts
@@ -2,17 +2,20 @@ import { App, Plugin } from 'vue'
 import Form from "./Form"
 import FormField from "./FormField"
 import FormGroup from './FormGroup'
+import FormTextarea from './FormTextarea'
 
 export default {
   install(app: App) {
     app.component(Form.name, Form)
     app.component(FormField.name, FormField)
     app.component(FormGroup.name, FormGroup)
+    app.component(FormTextarea.name, FormTextarea)
   }
 } as Plugin
 
 export {
   Form,
   FormField,
-  FormGroup
+  FormGroup,
+  FormTextarea
 }
