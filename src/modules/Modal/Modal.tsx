f1ded0444f682d0029de92da78ffc7836324d9f3
nightswinger <stardustkids83@gmail.com>
2021-05-09 02:02:10 +0000

    add animation

--- src/modules/Modal/Modal.tsx
+++ src/modules/Modal/Modal.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import { computed, defineComponent, Teleport } from "vue";
+import { computed, defineComponent, onMounted, onUnmounted, ref, Teleport, watch } from "vue";
 import { computeKeyOnly } from "../../utils/classNameHelper";
 
 export default defineComponent({
@@ -11,16 +11,58 @@ export default defineComponent({
     size: String
   },
   setup(props, { emit }) {
+    const visualState = ref(props.modelValue ? 'open' : 'closed')
+    const root = ref<Element | null>(null)
+
+    const isVisible = computed(() => visualState.value !== 'closed')
+
+    watch(() => props.modelValue, (newValue) => {
+      visualState.value = newValue ? 'opening' : 'closing'
+    })
+
+    const computeAnimationClass = (visualState: string, animationType = 'scale') => {
+      switch (visualState) {
+        case 'opening':
+          return `animating ${animationType} in`
+        case 'open':
+          return 'visible active'
+        case 'closing':
+          return `visible active animating ${animationType} out`
+        case 'closed':
+          return 'hidden'
+      }
+    }
+
+    const updateVisualState = () => visualState.value = props.modelValue ? 'open' : 'closed'
+
+    onMounted(() => {
+      root.value?.addEventListener('animationend', updateVisualState, true)
+    })
+
+    onUnmounted(() => {
+      root.value?.removeEventListener('animationend', updateVisualState)
+    })
+
     const dimmerClass = computed(() => {
       return clsx(
         'ui',
         'page modals dimmer transition',
-        computeKeyOnly(props.modelValue, 'active visible')
+        computeAnimationClass(visualState.value, 'fade')
       )
     })
 
     const dimmerStyle = computed(() => {
-      return { display: props.modelValue ? 'flex !important' : 'none !important' }
+      return {
+        display: isVisible.value ? 'flex !important' : 'none !important',
+        animationDuration: '500ms'
+      }
+    })
+
+    const modalStyle = computed(() => {
+      return {
+        display: isVisible.value ? 'block !important' : 'none !important',
+        animationDuration: '500ms'
+      }
     })
 
     const computedClass = computed(() => {
@@ -30,7 +72,7 @@ export default defineComponent({
         computeKeyOnly(props.basic, 'basic'),
         'modal',
         'transition',
-        computeKeyOnly(props.modelValue, 'active visible')
+        computeAnimationClass(visualState.value)
       )
     })
 
@@ -41,8 +83,10 @@ export default defineComponent({
     return {
       dimmerClass,
       dimmerStyle,
+      modalStyle,
       computedClass,
-      close
+      close,
+      root
     }
   },
   render() {
@@ -52,9 +96,11 @@ export default defineComponent({
           class={this.dimmerClass}
           style={this.dimmerStyle}
           onClick={this.close}
+          ref={(ref) => this.root = ref as Element}
         >
           <div
             class={this.computedClass}
+            style={this.modalStyle}
             onClick={(e) => e.stopPropagation()}
           >
             {this.closeIcon && (<i aria-hidden="true" class="close icon" onClick={this.close} />)}
