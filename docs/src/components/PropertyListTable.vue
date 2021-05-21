82f042b7b4e6bd47d8e0bfe1e52bfdc68e2bbaf1
nightswinger <stardustkids83@gmail.com>
2021-05-21 10:51:59 +0000

    add TabDoc

---
+++ docs/src/components/PropertyListTable.vue
@@ -0,0 +1,31 @@
+<template>
+  <sui-table celled>
+    <sui-table-header>
+      <sui-table-row>
+        <sui-table-header-cell>Name</sui-table-header-cell>
+        <sui-table-header-cell>Type</sui-table-header-cell>
+        <sui-table-header-cell>Default</sui-table-header-cell>
+        <sui-table-header-cell>Description</sui-table-header-cell>
+      </sui-table-row>
+    </sui-table-header>
+    <sui-table-body>
+      <sui-table-row v-for="prop in properties" :key="prop.name">
+        <sui-table-cell>{{ prop.name }}</sui-table-cell>
+        <sui-table-cell>{{ prop.type }}</sui-table-cell>
+        <sui-table-cell>{{ prop.default }}</sui-table-cell>
+        <sui-table-cell>{{ prop.description }}</sui-table-cell>
+      </sui-table-row>
+    </sui-table-body>
+  </sui-table>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'PropertyListTable',
+  props: {
+    properties: Array
+  }
+})
+</script>
