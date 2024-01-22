f2d59441eb5c3dd66305eecbc3c0192ff6dbfc6a
nightswinger <stardustkids83@gmail.com>
2024-01-22 05:39:43 +0000

    update eslint packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,14 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 module.exports = {
-  "env": {
-      "browser": true,
-      "es2021": true
-  },
-  "extends": [
-      "eslint:recommended",
-      "plugin:vue/essential",
-      "plugin:@typescript-eslint/recommended",
-      "standard"
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript'
   ],
-  "parserOptions": {
-      "ecmaVersion": "latest",
-      "parser": "@typescript-eslint/parser",
-      "sourceType": "module"
-  },
-  "plugins": [
-      "vue",
-      "@typescript-eslint"
-  ]
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
 }
