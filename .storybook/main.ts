2acc09b6a7f59d79a2df1de47db716e03153c7e4
nightswinger <stardustkids83@gmail.com>
2024-01-24 00:22:39 +0900

    add controls addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,6 +3,7 @@ import { StorybookConfig } from '@storybook/vue3-vite';
 const config: StorybookConfig = {
   framework: '@storybook/vue3-vite',
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-controls']
 };
 
 export default config;
