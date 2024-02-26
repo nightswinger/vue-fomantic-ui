8ece6957cc8db975db7cc8b90d4d67c48eed3d78
nightswinger <stardustkids83@gmail.com>
2024-02-26 23:22:32 +0900

    update container docs

---
+++ docs/src/components/elements/Container/TextContainerDoc.vue
@@ -0,0 +1,52 @@
+<template>
+  <DocSection label="Text Container" :code="code">
+    <template #description>
+      A container can reduce its maximum width to more naturally accommodate a single column of text
+    </template>
+    <template #example>
+      <SuiContainer text>
+        <SuiHeader as="h2">Header</SuiHeader>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+          ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
+          magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+          ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
+          quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
+          arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+          Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
+          dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
+          Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
+          Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
+          viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
+          Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
+        </p>
+      </SuiContainer>
+    </template>
+  </DocSection>
+</template>
+
+<script setup>
+import DocSection from '@/components/doc/DocSection.vue'
+
+import { SuiContainer, SuiHeader } from 'vue-fomantic-ui'
+
+const code = `<template>
+  <SuiContainer text>
+    <SuiHeader as="h2">Header</SuiHeader>
+    <p>
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+      ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
+      magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+      ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
+      quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
+      arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+      Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
+      dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
+      Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
+      Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
+      viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
+      Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
+    </p>
+  </SuiContainer>
+<\/template>`
+</script>
