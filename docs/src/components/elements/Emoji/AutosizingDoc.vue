1c9694295bea894eba38c148dd9acc8cb6df32a6
nightswinger <stardustkids83@gmail.com>
2024-02-27 10:17:13 +0900

    update emoji docs

---
+++ docs/src/components/elements/Emoji/AutosizingDoc.vue
@@ -0,0 +1,42 @@
+<template>
+  <DocSection label="Autosizing" :code="code">
+    <template #description>
+      If no specific size class is given, all emojis are automatically sized according to the current element font-size
+    </template>
+    <template #example>
+      <SuiHeader size="small">
+        Within a Header
+        <SuiEmoji name="relaxed" />
+      </SuiHeader>
+      <SuiButton as="div" size="large">
+        Within a Button
+        <SuiEmoji name="relaxed" />
+      </SuiButton>
+      <SuiLabel size="massive">
+        Within a Label
+        <SuiEmoji name="relaxed" />
+      </SuiLabel>
+    </template>
+  </DocSection>
+</template>
+
+<script setup>
+import DocSection from '@/components/doc/DocSection.vue'
+
+import { SuiEmoji, SuiHeader, SuiButton, SuiLabel } from 'vue-fomantic-ui'
+
+const code = `<template>
+  <SuiHeader size="small">
+    Within a Header
+    <SuiEmoji name="relaxed" />
+  </SuiHeader>
+  <SuiButton as="div" size="large">
+    Within a Button
+    <SuiEmoji name="relaxed" />
+  </SuiButton>
+  <SuiLabel size="massive">
+    Within a Label
+    <SuiEmoji name="relaxed" />
+  </SuiLabel>
+<\/template>`
+</script>
---
+++ docs/src/components/elements/Emoji/SizeDoc.vue
@@ -0,0 +1,26 @@
+<template>
+  <DocSection label="Size" :code="code">
+    <template #description>
+      An emoji can vary in size
+    </template>
+    <template #example>
+      <SuiEmoji name="relaxed" size="small" />
+      <SuiEmoji name="relaxed" size="medium" />
+      <SuiEmoji name="relaxed" size="large" />
+      <SuiEmoji name="relaxed" size="big" />
+    </template>
+  </DocSection>
+</template>
+
+<script setup>
+import DocSection from '@/components/doc/DocSection.vue'
+
+import { SuiEmoji } from 'vue-fomantic-ui'
+
+const code = `<template>
+  <SuiEmoji name="relaxed" size="small" />
+  <SuiEmoji name="relaxed" size="medium" />
+  <SuiEmoji name="relaxed" size="large" />
+  <SuiEmoji name="relaxed" size="big" />
+<\/template>`
+</script>
