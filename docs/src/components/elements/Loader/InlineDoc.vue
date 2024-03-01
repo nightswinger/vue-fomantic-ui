50e1afb71a1c485e82589cfea57c30f8fb0d0bbb
nightswinger <stardustkids83@gmail.com>
2024-03-01 21:05:53 +0900

    update loader docs

---
+++ docs/src/components/elements/Loader/InlineDoc.vue
@@ -0,0 +1,20 @@
+<template>
+  <DocSection label="Inline" :code="code">
+    <template #description>
+      Loaders can appear inline with content
+    </template>
+    <template #example>
+      <SuiLoader active inline />
+    </template>
+  </DocSection>
+</template>
+
+<script setup>
+import DocSection from '@/components/doc/DocSection.vue'
+
+import { SuiLoader } from 'vue-fomantic-ui'
+
+const code = `<template>
+  <SuiLoader active inline />
+</template>`
+</script>
---
+++ docs/src/components/elements/Loader/InvertedDoc.vue
@@ -0,0 +1,28 @@
+<template>
+  <DocSection label="Inverted" :code="code">
+    <template #description>
+      Loaders can have their colors inverted.
+    </template>
+    <template #example>
+      <SuiSegment inverted>
+        <SuiLoader active inverted />
+
+        <SuiImage src="/vue-fomantic-ui/images/wireframe/short-paragraph.png" />
+      </SuiSegment>
+    </template>
+  </DocSection>
+</template>
+
+<script setup>
+import DocSection from '@/components/doc/DocSection.vue'
+
+import { SuiLoader, SuiSegment, SuiDimmer, SuiImage } from 'vue-fomantic-ui'
+
+const code = `<template>
+  <SuiSegment inverted>
+    <SuiLoader active inverted />
+
+    <SuiImage src="/vue-fomantic-ui/images/wireframe/short-paragraph.png" />
+  </SuiSegment>
+</template>`
+</script>
