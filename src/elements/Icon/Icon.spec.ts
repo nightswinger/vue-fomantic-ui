0f4673a2a77afc0398bf1083dc44a53aa7699bb5
nightswinger <stardustkids83@gmail.com>
2024-03-02 16:32:12 +0900

    support circular colored

--- src/elements/Icon/Icon.spec.ts
+++ src/elements/Icon/Icon.spec.ts
@@ -27,4 +27,20 @@ describe('SuiIcon', () => {
       expect(wrapper.classes()).toContain('secondary');
     });
   })
+
+  describe('circular', () => {
+    describe('circular with colored', () => {
+      it('renders the circular colored class', async () => {
+        const wrapper = mount(Icon, {
+          props: {
+            circular: true,
+            colored: true,
+          }
+        });
+
+        expect(wrapper.classes()).toContain('circular');
+        expect(wrapper.classes()).toContain('colored');
+      });
+    })
+  })
 });
