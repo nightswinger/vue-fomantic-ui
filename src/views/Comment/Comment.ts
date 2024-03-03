f7500930981644abf1d3b95e27b6bd25c381e6f3
nightswinger <stardustkids83@gmail.com>
2024-03-03 17:29:44 +0900

    add named slots to Comment

--- src/views/Comment/Comment.ts
+++
@@ -1,8 +0,0 @@
-import { defineComponent, h } from "vue";
-
-export default defineComponent({
-  name: 'SuiComment',
-  render() {
-    return h('div', { class: 'comment' }, this.$slots.default?.())
-  }
-})
