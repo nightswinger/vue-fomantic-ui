10ac813661f6add935962f5708ba444e935e124a
nightswinger <stardustkids83@gmail.com>
2024-03-09 23:32:14 +0900

    add basic prop to SegmentGroup

--- src/components/Segment/SegmentGroup.tsx
+++ src/components/Segment/SegmentGroup.tsx
@@ -1,37 +1,40 @@
 import clsx from 'clsx'
 import { computed, defineComponent } from 'vue'
-import { computeKeyOnly } from '../../utils/classNameHelper'
 
-export default defineComponent({
+import { makeSizeProps, useSize } from '@/composables/size'
+import { computeKeyOnly } from '@/utils/classNameHelper'
+
+export default defineComponent((props, { slots }) => {
+  const { sizeClass } = useSize(props)
+
+  const classes = computed(() => {
+    return clsx(
+      'ui',
+      sizeClass.value,
+      computeKeyOnly(props.basic, 'basic'),
+      computeKeyOnly(props.compact, 'compact'),
+      computeKeyOnly(props.horizontal, 'horizontal'),
+      computeKeyOnly(props.piled, 'piled'),
+      computeKeyOnly(props.raised, 'raised'),
+      computeKeyOnly(props.stacked, 'stacked'),
+      'segments'
+    )
+  })
+
+  return () => (
+    <div class={classes.value}>
+      {slots.default?.()}
+    </div>
+  )
+},
+{
   props: {
+    basic: Boolean,
     compact: Boolean,
     horizontal: Boolean,
     piled: Boolean,
     raised: Boolean,
-    size: String,
-    stacked: Boolean
-  },
-  setup(props) {
-    const computedClass = computed(() => {
-      return clsx(
-        'ui',
-        props.size,
-        computeKeyOnly(props.compact, 'compact'),
-        computeKeyOnly(props.horizontal, 'horizontal'),
-        computeKeyOnly(props.piled, 'piled'),
-        computeKeyOnly(props.raised, 'raised'),
-        computeKeyOnly(props.stacked, 'stacked'),
-        'segments'
-      )
-    })
-
-    return {
-      computedClass
-    }
-  },
-  render() {
-    return (
-      <div class={this.computedClass}>{this.$slots.default?.()}</div>
-    )
+    stacked: Boolean,
+    ...makeSizeProps(),
   }
 })
