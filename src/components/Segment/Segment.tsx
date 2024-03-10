109cc865c5cb518e09cc6c581ebcaa57343f93b2
nightswinger <stardustkids83@gmail.com>
2024-03-10 09:01:17 +0900

    add scrolling prop to Segment

--- src/components/Segment/Segment.tsx
+++ src/components/Segment/Segment.tsx
@@ -1,14 +1,57 @@
 import clsx from 'clsx'
 import { computed, defineComponent } from 'vue'
-import { computeKeyOnly, computeKeyOrKeyValue, computeKeyValue } from '../../utils/classNameHelper'
 
-export default defineComponent({
+import type { PropType } from 'vue'
+
+import { makeColorProps, useColor } from '@/composables/color'
+import { makeSizeProps, useSize } from '@/composables/size'
+import { computeKeyOnly, computeKeyOrKeyValue, computeKeyValue } from '@/utils/classNameHelper'
+
+export default defineComponent((props, { slots }) => {
+  const { colorClasses } = useColor(props)
+  const { sizeClass } = useSize(props)
+
+  const classes = computed(() => {
+    return clsx(
+      'ui',
+      colorClasses.value,
+      sizeClass.value,
+      computeKeyOnly(props.basic, 'basic'),
+      computeKeyOnly(props.circular, 'circular'),
+      computeKeyOnly(props.clearing, 'clearing'),
+      computeKeyOnly(props.compact, 'compact'),
+      computeKeyOnly(props.disabled, 'disabled'),
+      computeKeyOnly(props.inverted, 'inverted'),
+      computeKeyOnly(props.loading, 'loading'),
+      computeKeyOnly(props.piled, 'piled'),
+      computeKeyOnly(props.placeholder, 'placeholder'),
+      computeKeyOnly(props.raised, 'raised'),
+      computeKeyOnly(props.secondary, 'secondary'),
+      computeKeyOnly(props.stacked, 'stacked'),
+      computeKeyOnly(props.tertiary, 'tertiary'),
+      computeKeyOnly(props.vertical, 'vertical'),
+      computeKeyValue(props.floated, 'floated'),
+      computeKeyValue(props.textAlign, 'aligned'),
+      computeKeyOrKeyValue(props.attached, 'attached'),
+      computeKeyOrKeyValue(props.fitted, 'fitted'),
+      computeKeyOrKeyValue(props.padded, 'padded'),
+      computeKeyOrKeyValue(props.scrolling, 'scrolling'),
+      'segment'
+    )
+  })
+
+  return () => (
+    <div class={classes.value}>
+      {slots.default?.()}
+    </div>
+  )
+},
+{
   props: {
     attached: [Boolean, String],
     basic: Boolean,
     circular: Boolean,
     clearing: Boolean,
-    color: String,
     compact: Boolean,
     disabled: Boolean,
     fitted: [Boolean, String],
@@ -19,49 +62,14 @@ export default defineComponent({
     piled: Boolean,
     placeholder: Boolean,
     raised: Boolean,
+    scrolling: [Boolean, String] as PropType<boolean | 'very long' | 'long' | 'short' | 'very short'>,
     secondary: Boolean,
-    size: String,
     stacked: Boolean,
     tertiary: Boolean,
     textAlign: String,
     vertical: Boolean,
-  },
-  setup(props) {
-    const computedClass = computed(() => {
-      return clsx(
-        'ui',
-        props.color,
-        props.size,
-        computeKeyOnly(props.basic, 'basic'),
-        computeKeyOnly(props.circular, 'circular'),
-        computeKeyOnly(props.clearing, 'clearing'),
-        computeKeyOnly(props.compact, 'compact'),
-        computeKeyOnly(props.disabled, 'disabled'),
-        computeKeyOnly(props.inverted, 'inverted'),
-        computeKeyOnly(props.loading, 'loading'),
-        computeKeyOnly(props.piled, 'piled'),
-        computeKeyOnly(props.placeholder, 'placeholder'),
-        computeKeyOnly(props.raised, 'raised'),
-        computeKeyOnly(props.secondary, 'secondary'),
-        computeKeyOnly(props.stacked, 'stacked'),
-        computeKeyOnly(props.tertiary, 'tertiary'),
-        computeKeyOnly(props.vertical, 'vertical'),
-        computeKeyValue(props.floated, 'floated'),
-        computeKeyValue(props.textAlign, 'aligned'),
-        computeKeyOrKeyValue(props.attached, 'attached'),
-        computeKeyOrKeyValue(props.fitted, 'fitted'),
-        computeKeyOrKeyValue(props.padded, 'padded'),
-        'segment'
-      )
-    })
 
-    return { computedClass }
-  },
-  render() {
-    return (
-      <div class={this.computedClass}>
-        {this.$slots.default?.()}
-      </div>
-    )
+    ...makeColorProps(),
+    ...makeSizeProps(),
   }
 })
