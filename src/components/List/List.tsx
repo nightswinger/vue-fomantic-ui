89a12c252512575dcb05a6d139e0b744c4b6d6bd
nightswinger <stardustkids83@gmail.com>
2024-03-14 01:00:05 +0000

    refactoring List

---
+++ src/components/List/List.tsx
@@ -0,0 +1,50 @@
+import clsx from "clsx"
+import { computed, defineComponent, h } from "vue"
+
+import { computeKeyOnly, computeKeyValue } from "@/utils/classNameHelper"
+
+export default defineComponent((props, { slots }) => {
+  const classes = computed(() => {
+    return clsx(
+      'ui',
+      computeKeyOnly(props.animated, 'animated'),
+      computeKeyOnly(props.bulleted, 'bulleted'),
+      computeKeyOnly(props.celled, 'celled'),
+      computeKeyOnly(props.divided, 'divided'),
+      computeKeyOnly(props.horizontal, 'horizontal'),
+      computeKeyOnly(props.inverted, 'inverted'),
+      computeKeyOnly(props.link, 'link'),
+      computeKeyOnly(props.ordered, 'ordered'),
+      computeKeyOnly(props.relaxed, 'relaxed'),
+      computeKeyOnly(props.selection, 'selection'),
+      computeKeyValue(props.verticalAlign, 'aligned'),
+      computeKeyValue(props.floated, 'floated'),
+      'list'
+    )
+  })
+
+  let elementType = props.as || 'div'
+
+  return () => (
+    h(elementType, {
+      class: classes.value
+    }, slots.default?.())
+  )
+},
+{
+  props: {
+    animated: Boolean,
+    as: String,
+    bulleted: Boolean,
+    celled: Boolean,
+    divided: Boolean,
+    floated: String,
+    horizontal: Boolean,
+    inverted: Boolean,
+    ordered: Boolean,
+    relaxed: Boolean,
+    link: Boolean,
+    selection: Boolean,
+    verticalAlign: String,
+  },
+})
