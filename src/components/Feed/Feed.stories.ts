f64910cdf353bf4909fb250243e662313fdf69d7
nightswinger <stardustkids83@gmail.com>
2024-03-14 06:29:38 +0000

    add slots extra to FeedEvent

--- src/components/Feed/Feed.stories.ts
+++ src/components/Feed/Feed.stories.ts
@@ -24,12 +24,89 @@ export const Default: Story = {
               Elliot Fu
             </a> added you as a friend
             <div class="date">
-              1 Hour Ago
+              1 hour ago
             </div>
           </template>
           <template #meta>
             <a class="like">
               <i class="like icon"></i> 4 Likes
+            </a>
+          </template>
+        </FeedEvent>
+        <FeedEvent>
+          <template #label>
+            <img src="./avatar/small/helen.jpg">
+          </template>
+          <template #summary>
+            <a>Helen Troy</a> added <a>2 new illustrations</a>
+            <div class="date">
+              4 days ago
+            </div>
+          </template>
+          <template #extraImages>
+            <a><img src="./wireframe/image.png"></a>
+            <a><img src="./wireframe/image.png"></a>
+          </template>
+          <template #meta>
+            <a class="like">
+              <i class="like icon"></i> 1 Like
+            </a>
+          </template>
+        </FeedEvent>
+        <FeedEvent>
+          <template #label>
+            <img src="./avatar/small/jenny.jpg">
+          </template>
+          <template #summary>
+            <a class="user">
+              Jenny Hess
+            </a> added you as a friend
+            <div class="date">
+              2 days ago
+            </div>
+          </template>
+          <template #meta>
+            <a class="like">
+              <i class="like icon"></i> 8 Likes
+            </a>
+          </template>
+        </FeedEvent>
+        <FeedEvent>
+          <template #label>
+            <img src="./avatar/small/joe.jpg">
+          </template>
+          <template #summary>
+            <a>Joe Henderson</a> posted on his page
+            <div class="date">
+              3 days ago
+            </div>
+          </template>
+          <template #extraText>
+            Ours is a life of constant reruns. We're always circling back to where we'd we started, then starting all over again. Even if we don't run extra laps that day, we surely will come back for more of the same another day soon.
+          </template>
+          <template #meta>
+            <a class="like">
+              <i class="like icon"></i> 5 Likes
+            </a>
+          </template>
+        </FeedEvent>
+        <FeedEvent>
+          <template #label>
+            <img src="./avatar/small/justen.jpg">
+          </template>
+          <template #summary>
+            <a>Justen Kitsune</a> added <a>2 new photos</a> of you
+            <div class="date">
+              4 days ago
+            </div>
+          </template>
+          <template #extraImages>
+            <a><img src="./wireframe/image.png"></a>
+            <a><img src="./wireframe/image.png"></a>
+          </template>
+          <template #meta>
+            <a class="like">
+              <i class="like icon"></i> 41 Like
             </a>
           </template>
         </FeedEvent>
--- src/components/Feed/FeedEvent.tsx
+++ src/components/Feed/FeedEvent.tsx
@@ -8,6 +8,8 @@ export default defineComponent({
         (this.$slots.summary || this.$slots.meta) &&
         <div class="content">
           {this.$slots.summary && <div class="summary">{this.$slots.summary()}</div>}
+          {this.$slots.extraText && <div class="extra text">{this.$slots.extraText()}</div>}
+          {this.$slots.extraImages && <div class="extra images">{this.$slots.extraImages()}</div>}
           {this.$slots.meta && <div class="meta">{this.$slots.meta()}</div>}
         </div>
       }
