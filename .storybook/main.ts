1ded836e29801d8ecada2477a0ba6ff13c16fdc1
nightswinger <stardustkids83@gmail.com>
2024-03-14 03:01:38 +0000

    upgrade storybook to v8

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,9 +2,13 @@ import { StorybookConfig } from '@storybook/vue3-vite';
 
 const config: StorybookConfig = {
   framework: '@storybook/vue3-vite',
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  stories: ['../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
   addons: ['@storybook/addon-controls', '@storybook/addon-actions'],
   staticDirs: ['./assets', '../docs/public/images'],
+
+  docs: {
+    autodocs: true
+  }
 };
 
 export default config;
