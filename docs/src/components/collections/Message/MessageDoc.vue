37a53f315a210e8f0c875b258fc4e5d5949368a5
nightswinger <stardustkids83@gmail.com>
2024-03-23 09:34:30 +0900

    add MessageDoc

---
+++ docs/src/components/collections/Message/MessageDoc.vue
@@ -0,0 +1,26 @@
+<template>
+  <DocSection label="Message" :code="code">
+    <template #description>
+      A basic message
+    </template>
+    <template #example>
+      <Message
+        header="Changes in Service"
+        content="We updated our privacy policy here to better service our customers. We recommend reviewing the changes."
+      />
+    </template>
+  </DocSection>
+</template>
+
+<script setup>
+import DocSection from '@/components/doc/DocSection.vue'
+
+import { Message } from 'vue-fomantic-ui'
+
+const code = `<template>
+  <Message
+    header="Changes in Service"
+    content="We updated our privacy policy here to better service our customers. We recommend reviewing the changes."
+  />
+<\/template>`
+</script>
---
+++ docs/src/pages/collections/Message.vue
@@ -0,0 +1,23 @@
+<template>
+  <DocComponent
+    title="Message"
+    description="A message displays information that explains nearby content"
+    :component-docs="docs"
+  />
+</template>
+
+<script setup>
+import DocComponent from '@/components/doc/DocComponent.vue'
+
+// Types
+import MessageDoc from '@/components/collections/Message/MessageDoc.vue'
+
+const docs = [
+  {
+    id: 'message',
+    label: 'Message',
+    category: 'Types',
+    component: MessageDoc,
+  }
+]
+</script>
