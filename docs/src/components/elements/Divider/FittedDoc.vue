93940344fd7f523fe57a0ee9eb36b31fa47fc389
nightswinger <stardustkids83@gmail.com>
2024-04-11 01:06:11 +0000

    update FittedDoc for Divider

--- docs/src/components/elements/Divider/FittedDoc.vue
+++ docs/src/components/elements/Divider/FittedDoc.vue
@@ -1,28 +1,28 @@
 <template>
-  <DocSection label="Inverted" :code="code">
+  <DocSection label="Fitted" :code="code">
     <template #description>
       A divider can be fitted, without any space above or below it.
     </template>
     <template #example>
-      <SuiSegment>
+      <Segment>
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.
-        <SuiDivider fitted />
+        <Divider fitted />
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.
-      </SuiSegment>
+      </Segment>
     </template>
   </DocSection>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import DocSection from '@/components/doc/DocSection.vue'
 
-import { SuiDivider, SuiSegment } from 'vue-fomantic-ui'
+import { Divider, Segment } from 'vue-fomantic-ui'
 
 const code = `<template>
-  <SuiSegment>
+  <Segment>
     Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.
-    <SuiDivider fitted />
+    <Divider fitted />
     Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.
-  </SuiSegment>
+  </Segment>
 </template>`
 </script>
