d8bf22fe8f46a471a108fa2e564bb7ae0624f641
nightswinger <stardustkids83@gmail.com>
2024-04-22 08:08:46 +0000

    add api docs

--- docs/src/pages/collections/Grid.vue
+++ docs/src/pages/collections/Grid.vue
@@ -3,6 +3,7 @@
     title="Grid"
     description="A grid is used to harmonize negative space in a layout"
     :component-docs="docs"
+    :api-docs="api"
   />
 </template>
 
@@ -180,4 +181,53 @@ const docs = [
     component: ResponsiveWidthDoc,
   },
 ]
+
+const api = {
+  Grid: {
+    props: [
+      { name: 'celled', type: 'boolean|string', default: 'false', description: 'A grid can have rows divided into cells.' },
+      { name: 'centered', type: 'boolean', default: 'false', description: 'A grid can have its columns centered.' },
+      { name: 'columns', type: 'number|string', default: 'undefined', description: 'Represents column count per row in Grid.' },
+      { name: 'container', type: 'boolean', default: 'false', description: 'A grid can be combined with a container to use the available layout and alignment.' },
+      { name: 'divided', type: 'boolean|string', default: 'false', description: 'A grid can have dividers between its columns.' },
+      { name: 'doubling', type: 'boolean', default: 'false', description: 'A grid can double its column width on tablet and mobile sizes.' },
+      { name: 'inverted', type: 'boolean', default: 'false', description: 'A grid\'s colors can be inverted.' },
+      { name: 'padded', type: 'boolean|string', default: 'false', description: 'A grid can preserve its vertical and horizontal gutters on first and last columns.' },
+      { name: 'relaxed', type: 'boolean|string', default: 'false', description: 'A grid can increase its gutters to allow for more negative space.' },
+      { name: 'reversed', type: 'string', default: 'undefined', description: 'A grid can specify that its columns should reverse order at different device sizes.' },
+      { name: 'stackable', type: 'boolean', default: 'false', description: 'A grid can have its columns stack on-top of each other after reaching mobile breakpoints.' },
+      { name: 'stretched', type: 'boolean', default: 'false', description: 'A grid can stretch its contents to take up the entire grid height.' },
+      { name: 'textAlign', type: 'string', default: 'undefined', description: 'A grid can specify its text alignment.' },
+      { name: 'verticalAlign', type: 'string', default: 'undefined', description: 'A grid can specify its vertical alignment to have all its columns vertically centered.' },
+    ]
+  },
+  GridRow: {
+    props: [
+      { name: 'centered', type: 'boolean', default: 'false', description: 'A row can have its columns centered.' },
+      { name: 'color', type: 'string', default: 'undefined', description: 'A grid row can be colored. ' },
+      { name: 'divided', type: 'boolean', default: 'false', description: 'A row can have dividers between its columns.' },
+      { name: 'only', type: 'string', default: 'undefined', description: 'A row can appear only for a specific device, or screen sizes.' },
+      { name: 'reversed', type: 'string', default: 'undefined', description: 'A row can specify that its columns should reverse order at different device sizes.' },
+      { name: 'stretched', type: 'boolean', default: 'false', description: 'A row can stretch its contents to take up the entire column height.' },
+      { name: 'textAlign', type: 'string', default: 'undefined', description: 'A row can specify its text alignment. ' },
+      { name: 'verticalAlign', type: 'string', default: 'undefined', description: 'A row can specify its vertical alignment to have all its columns vertically centered. ' }
+    ]
+  },
+  GridColumn: {
+    props: [
+      { name: 'color', type: 'string', default: 'undefined', description: 'A grid column can be colored. ' },
+      { name: 'computer', type: 'number', default: 'undefined', description: 'A column can specify a width for a computer.' },
+      { name: 'floated', type: 'string', default: 'undefined', description: 'A column can sit flush against the left or right edge of a row. ' },
+      { name: 'largeScreen', type: 'number', default: 'undefined', description: 'A column can specify a width for a large screen device.' },
+      { name: 'mobile', type: 'number', default: 'undefined', description: 'A column can specify a width for a mobile device.' },
+      { name: 'only', type: 'string', default: 'undefined', description: 'A column can appear only for a specific device, or screen sizes.' },
+      { name: 'stretched', type: 'boolean', default: 'false', description: 'A column can stretch its contents to take up the entire grid or row height.' },
+      { name: 'tablet', type: 'number', default: 'undefined', description: 'A column can specify a width for a tablet device.' },
+      { name: 'textAlign', type: 'string', default: 'undefined', description: 'A column can specify its text alignment. ' },
+      { name: 'verticalAlign', type: 'string', default: 'undefined', description: 'A column can specify its vertical alignment to have all its columns vertically centered. ' },
+      { name: 'widescreen', type: 'number', default: 'undefined', description: 'A column can specify a width for a wide screen device.' },
+      { name: 'width', type: 'number', default: 'undefined', description: 'Represents width of column.' }
+    ]
+  }
+}
 </script>
