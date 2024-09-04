fdbe20d858cfedf30b847ecaf58399e0cf1f2061
nightswinger <nightswinger@users.noreply.github.com>
2024-09-04 12:02:37 +0000

    deploy: b5492c1f1708253bca5a21d29301ec1b39303995

---
+++ assets/Breadcrumb-LjyQktzH.js
@@ -0,0 +1,65 @@
+import{o as n,c as d,w as e,a as t,b as r,u as i,a1 as o,a2 as a,a3 as c,d as B,j as f,v as b,F as _,x as v,H as D,D as k}from"./index-BPzz6yRK.js";import{_ as s}from"./DocSection-CUzZAhfI.js";const h=`<template>
+  <SuiBreadcrumb
+    :sections="[
+      { content: 'Home', link: true },
+      { content: 'Store', link: true },
+      { content: 'T-Shirt', active: true }
+    ]"
+    icon="right angle"
+  />
+</template>`,g={__name:"BreadcrumbDoc",setup(l){return(u,m)=>(n(),d(s,{label:"Breadcrumb",code:h},{description:e(()=>[t(" A standard breadcrumb ")]),example:e(()=>[r(i(o),{sections:[{content:"Home",link:!0},{content:"Store",link:!0},{content:"T-Shirt",active:!0}],icon:"right angle"})]),_:1}))}},x=`<template>
+  <SuiBreadcrumb>
+    <SuiBreadcrumbSection link>Home</SuiBreadcrumbSection>
+    <SuiBreadcrumbDivider> / </SuiBreadcrumbDivider>
+    <SuiBreadcrumbSection link>Registration</SuiBreadcrumbSection>
+    <SuiBreadcrumbDivider> / </SuiBreadcrumbDivider>
+    <SuiBreadcrumbSection active>Personal Information</SuiBreadcrumbSection>
+  </SuiBreadcrumb>
+</template>`,H={__name:"DividerDoc",setup(l){return(u,m)=>(n(),d(s,{label:"Divider",code:x},{description:e(()=>[t(" A breadcrumb can contain a divider to show the relationship between sections, this can be formatted as an icon or text. ")]),example:e(()=>[r(i(o),null,{default:e(()=>[r(i(a),{link:""},{default:e(()=>[t("Home")]),_:1}),r(i(c),null,{default:e(()=>[t(" / ")]),_:1}),r(i(a),{link:""},{default:e(()=>[t("Registration")]),_:1}),r(i(c),null,{default:e(()=>[t(" / ")]),_:1}),r(i(a),{active:""},{default:e(()=>[t("Personal Information")]),_:1})]),_:1})]),_:1}))}},$=`<template>
+  <SuiBreadcrumb>
+    <SuiBreadcrumbSection link>Home</SuiBreadcrumbSection>
+    <SuiBreadcrumbDivider> / </SuiBreadcrumbDivider>
+    <SuiBreadcrumbSection active>Search</SuiBreadcrumbSection>
+  </SuiBreadcrumb>
+</template>`,y={__name:"SectionDoc",setup(l){return(u,m)=>(n(),d(s,{label:"Section",code:$},{description:e(()=>[t(" A breadcrumb can contain sections that can either be formatted as a link or text ")]),example:e(()=>[r(i(o),null,{default:e(()=>[r(i(a),{link:""},{default:e(()=>[t("Home")]),_:1}),r(i(c),null,{default:e(()=>[t(" / ")]),_:1}),r(i(a),{active:""},{default:e(()=>[t("Search")]),_:1})]),_:1})]),_:1}))}},A=B("a",{href:"#"},"paper towels",-1),P=`<template>
+  <SuiBreadcrumb>
+    <SuiBreadcrumbSection link>Home</SuiBreadcrumbSection>
+    <SuiBreadcrumbDivider> / </SuiBreadcrumbDivider>
+    <SuiBreadcrumbSection active>Search for: <a href="#">paper towels</a></SuiBreadcrumbSection>
+  </SuiBreadcrumb>
+</template>`,z={__name:"LinkDoc",setup(l){return(u,m)=>(n(),d(s,{label:"Link",code:P},{description:e(()=>[t(" A section may be linkable or contain a link ")]),example:e(()=>[r(i(o),null,{default:e(()=>[r(i(a),{link:""},{default:e(()=>[t("Home")]),_:1}),r(i(c),null,{default:e(()=>[t(" / ")]),_:1}),r(i(a),{active:""},{default:e(()=>[t("Search for: "),A]),_:1})]),_:1})]),_:1}))}},I=`<template>
+  <SuiBreadcrumb>
+    <SuiBreadcrumbSection link>Products</SuiBreadcrumbSection>
+    <SuiBreadcrumbDivider> / </SuiBreadcrumbDivider>
+    <SuiBreadcrumbSection active>Paper Towels</SuiBreadcrumbSection>
+  </SuiBreadcrumb>
+</template>`,w={__name:"ActiveDoc",setup(l){return(u,m)=>(n(),d(s,{label:"Active",code:I},{description:e(()=>[t(" A section can be active ")]),example:e(()=>[r(i(o),null,{default:e(()=>[r(i(a),{link:""},{default:e(()=>[t("Products")]),_:1}),r(i(c),null,{default:e(()=>[t(" / ")]),_:1}),r(i(a),{active:""},{default:e(()=>[t("Paper Towels")]),_:1})]),_:1})]),_:1}))}},R=B("br",null,null,-1),T=`<template>
+  <SuiSegment inverted>
+    <SuiBreadcrumb inverted>
+      <SuiBreadcrumbSection link>Home</SuiBreadcrumbSection>
+      <SuiBreadcrumbDivider> / </SuiBreadcrumbDivider>
+      <SuiBreadcrumbSection link>Registration</SuiBreadcrumbSection>
+      <SuiBreadcrumbDivider> / </SuiBreadcrumbDivider>
+      <SuiBreadcrumbSection active>Personal Information</SuiBreadcrumbSection>
+    </SuiBreadcrumb>
+    <br>
+    <SuiBreadcrumb inverted>
+      <SuiBreadcrumbSection>Home</SuiBreadcrumbSection>
+      <SuiBreadcrumbDivider> / </SuiBreadcrumbDivider>
+      <SuiBreadcrumbSection>Registration</SuiBreadcrumbSection>
+      <SuiBreadcrumbDivider> / </SuiBreadcrumbDivider>
+      <SuiBreadcrumbSection active>Personal Information</SuiBreadcrumbSection>
+    </SuiBreadcrumb>
+  </SuiSegment>
+</template>`,C={__name:"InvertedDoc",setup(l){return(u,m)=>(n(),d(s,{label:"Inverted",code:T},{description:e(()=>[t(" A breadcrumb can be inverted ")]),example:e(()=>[r(i(f),{inverted:""},{default:e(()=>[r(i(o),{inverted:""},{default:e(()=>[r(i(a),{link:""},{default:e(()=>[t("Home")]),_:1}),r(i(c),null,{default:e(()=>[t(" / ")]),_:1}),r(i(a),{link:""},{default:e(()=>[t("Registration")]),_:1}),r(i(c),null,{default:e(()=>[t(" / ")]),_:1}),r(i(a),{active:""},{default:e(()=>[t("Personal Information")]),_:1})]),_:1}),R,r(i(o),{inverted:""},{default:e(()=>[r(i(a),null,{default:e(()=>[t("Home")]),_:1}),r(i(c),null,{default:e(()=>[t(" / ")]),_:1}),r(i(a),null,{default:e(()=>[t("Registration")]),_:1}),r(i(c),null,{default:e(()=>[t(" / ")]),_:1}),r(i(a),{active:""},{default:e(()=>[t("Personal Information")]),_:1})]),_:1})]),_:1})]),_:1}))}},V=`<template>
+  <template v-for="size in ['mini', 'tiny', 'small', 'large', 'big', 'huge', 'massive']" :key="size">
+    <SuiBreadcrumb :size="size">
+      <SuiBreadcrumbSection link>Home</SuiBreadcrumbSection>
+      <SuiBreadcrumbDivider> / </SuiBreadcrumbDivider>
+      <SuiBreadcrumbSection link>Registration</SuiBreadcrumbSection>
+      <SuiBreadcrumbDivider> / </SuiBreadcrumbDivider>
+      <SuiBreadcrumbSection active>Personal Information</SuiBreadcrumbSection>
+    </SuiBreadcrumb>
+    <SuiDivider hidden />
+  </template>
+</template>`,L={__name:"SizeDoc",setup(l){const u=["mini","tiny","small","large","big","huge","massive"];return(m,p)=>(n(),d(s,{label:"Size",code:V},{description:e(()=>[t(" A breadcrumb can vary in size ")]),example:e(()=>[(n(),b(_,null,v(u,S=>(n(),b(_,{key:S},[r(i(o),{size:S},{default:e(()=>[r(i(a),{link:""},{default:e(()=>[t("Home")]),_:1}),r(i(c),null,{default:e(()=>[t(" / ")]),_:1}),r(i(a),{link:""},{default:e(()=>[t("Registration")]),_:1}),r(i(c),null,{default:e(()=>[t(" / ")]),_:1}),r(i(a),{active:""},{default:e(()=>[t("Personal Information")]),_:1})]),_:2},1032,["size"]),r(i(D),{hidden:""})],64))),64))]),_:1}))}},F={__name:"Breadcrumb",setup(l){const u=[{id:"breadcrumb",label:"Breadcrumb",category:"Types",component:g},{id:"divider",label:"Divider",category:"Content",component:H},{id:"section",label:"Section",category:"Content",component:y},{id:"link",label:"Link",category:"Content",component:z},{id:"active",label:"Active",category:"States",component:w},{id:"inverted",label:"Inverted",category:"Variations",component:C},{id:"size",label:"Size",category:"Variations",component:L}];return(m,p)=>(n(),d(k,{title:"Breadcrumb",description:"A standard breadcrumb","component-docs":u}))}};export{F as default};
---
+++ assets/Reveal-BACIaX54.js
@@ -0,0 +1,55 @@
+import{o as l,c as m,w as e,a as c,b as a,u as t,S as u,T as i,K as n,D as v}from"./index-BPzz6yRK.js";import{_ as d}from"./DocSection-CUzZAhfI.js";const p=`<template>
+  <SuiReveal animated="fade">
+    <SuiRevealContent visible>
+      <SuiImage size="small" src="/images/wireframe/square-image.png" />
+    </SuiRevealContent>
+    <SuiRevealContent hidden>
+      <SuiImage size="small" src="/images/avatar/large/ade.jpg" />
+    </SuiRevealContent>
+  </SuiReveal>
+</template>`,g={__name:"FadeDoc",setup(r){return(s,o)=>(l(),m(d,{label:"Fade",code:p},{description:e(()=>[c(" An element can disappear to reveal content below ")]),example:e(()=>[a(t(u),{animated:"fade"},{default:e(()=>[a(t(i),{visible:""},{default:e(()=>[a(t(n),{size:"small",src:"/vue-fomantic-ui/images/wireframe/square-image.png"})]),_:1}),a(t(i),{hidden:""},{default:e(()=>[a(t(n),{size:"small",src:"/vue-fomantic-ui/images/avatar/large/ade.jpg"})]),_:1})]),_:1})]),_:1}))}},_=`<template>
+  <SuiReveal animated="move">
+    <SuiRevealContent visible>
+      <SuiImage size="small" src="/images/wireframe/square-image.png" />
+    </SuiRevealContent>
+    <SuiRevealContent hidden>
+      <SuiImage size="small" src="/images/avatar/large/chris.jpg" />
+    </SuiRevealContent>
+  </SuiReveal>
+</template>`,f={__name:"MoveDoc",setup(r){return(s,o)=>(l(),m(d,{label:"Move",code:_},{description:e(()=>[c(" An element can move in a direction to reveal content ")]),example:e(()=>[a(t(u),{animated:"move"},{default:e(()=>[a(t(i),{visible:""},{default:e(()=>[a(t(n),{size:"small",src:"/vue-fomantic-ui/images/wireframe/square-image.png"})]),_:1}),a(t(i),{hidden:""},{default:e(()=>[a(t(n),{size:"small",src:"/vue-fomantic-ui/images/avatar/large/chris.jpg"})]),_:1})]),_:1})]),_:1}))}},S=`<template>
+  <SuiReveal animated="rotate">
+    <SuiRevealContent visible>
+      <SuiImage size="small" src="/images/wireframe/square-image.png" />
+    </SuiRevealContent>
+    <SuiRevealContent hidden>
+      <SuiImage size="small" src="/images/avatar/large/stevie.jpg" />
+    </SuiRevealContent>
+  </SuiReveal>
+</template>`,R={__name:"RotateDoc",setup(r){return(s,o)=>(l(),m(d,{label:"Rotate",code:S},{description:e(()=>[c(" An element can rotate to reveal content below ")]),example:e(()=>[a(t(u),{animated:"rotate"},{default:e(()=>[a(t(i),{visible:""},{default:e(()=>[a(t(n),{size:"small",src:"/vue-fomantic-ui/images/wireframe/square-image.png"})]),_:1}),a(t(i),{hidden:""},{default:e(()=>[a(t(n),{size:"small",src:"/vue-fomantic-ui/images/avatar/large/stevie.jpg"})]),_:1})]),_:1})]),_:1}))}},b=`<template>
+  <SuiReveal active animated="move">
+    <SuiRevealContent visible>
+      <SuiImage size="small" src="/images/wireframe/square-image.png" />
+    </SuiRevealContent>
+    <SuiRevealContent hidden>
+      <SuiImage size="small" src="/images/avatar/large/nan.jpg" />
+    </SuiRevealContent>
+  </SuiReveal>
+</template>`,h={__name:"ActiveDoc",setup(r){return(s,o)=>(l(),m(d,{label:"Active",code:b},{description:e(()=>[c(" An active reveal displays its hidden content ")]),example:e(()=>[a(t(u),{active:"",animated:"move"},{default:e(()=>[a(t(i),{visible:""},{default:e(()=>[a(t(n),{size:"small",src:"/vue-fomantic-ui/images/wireframe/square-image.png"})]),_:1}),a(t(i),{hidden:""},{default:e(()=>[a(t(n),{size:"small",src:"/vue-fomantic-ui/images/avatar/large/nan.jpg"})]),_:1})]),_:1})]),_:1}))}},C=`<template>
+  <SuiReveal disabled animated="move">
+    <SuiRevealContent visible>
+      <SuiImage size="small" src="/images/wireframe/square-image.png" />
+    </SuiRevealContent>
+    <SuiRevealContent hidden>
+      <SuiImage size="small" src="/images/avatar/large/nan.jpg" />
+    </SuiRevealContent>
+  </SuiReveal>
+</template>`,z={__name:"DisabledDoc",setup(r){return(s,o)=>(l(),m(d,{label:"Disabled",code:C},{description:e(()=>[c(" A disabled reveal will not animate when hovered ")]),example:e(()=>[a(t(u),{disabled:"",animated:"move"},{default:e(()=>[a(t(i),{visible:""},{default:e(()=>[a(t(n),{size:"small",src:"/vue-fomantic-ui/images/wireframe/square-image.png"})]),_:1}),a(t(i),{hidden:""},{default:e(()=>[a(t(n),{size:"small",src:"/vue-fomantic-ui/images/avatar/large/nan.jpg"})]),_:1})]),_:1})]),_:1}))}},w=`<template>
+  <SuiReveal instant animated="move">
+    <SuiRevealContent visible>
+      <SuiImage size="small" src="/images/wireframe/square-image.png" />
+    </SuiRevealContent>
+    <SuiRevealContent hidden>
+      <SuiImage size="small" src="/images/avatar/large/chris.jpg" />
+    </SuiRevealContent>
+  </SuiReveal>
+</template>`,x={__name:"InstantDoc",setup(r){return(s,o)=>(l(),m(d,{label:"Instant",code:w},{description:e(()=>[c(" An element can show its content without delay ")]),example:e(()=>[a(t(u),{instant:"",animated:"move"},{default:e(()=>[a(t(i),{visible:""},{default:e(()=>[a(t(n),{size:"small",src:"/vue-fomantic-ui/images/wireframe/square-image.png"})]),_:1}),a(t(i),{hidden:""},{default:e(()=>[a(t(n),{size:"small",src:"/vue-fomantic-ui/images/avatar/large/chris.jpg"})]),_:1})]),_:1})]),_:1}))}},y={__name:"Reveal",setup(r){const s=[{id:"fade",label:"Fade",category:"Types",component:g},{id:"move",label:"Move",category:"Types",component:f},{id:"rotate",label:"Rotate",category:"Types",component:R},{id:"active",label:"Active",category:"States",component:h},{id:"disabled",label:"Disabled",category:"States",component:z},{id:"instant",label:"Instant",category:"Variations",component:x}];return(o,I)=>(l(),m(v,{title:"Reveal",description:"A reveal displays additional content in place of previous content when activated","component-docs":s}))}};export{y as default};
